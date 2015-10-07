144a4f17e2dbdef57c5b9259ebb8c447bd111223
EC2 Default User <ec2-user@ip-172-31-19-185.us-west-2.compute.internal>
2015-10-07 04:02:32 +0000

    add schema

---
+++ app/schema.js
@@ -0,0 +1,27 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var userSchema = new Schema({
+  userName: String,
+  pushId: String,
+  questions: [{ type: Schema.Types.ObjectId, ref: 'Question'}],
+  answers: [{ type: Schema.Types.ObjectId, ref: 'Answer' }]
+});
+
+var questionSchema = new Schema({
+  imageUrl: String,
+  questionText: String, 
+  answers: [{ type: Schema.Types.ObjectId, ref: 'Answer' }]
+});
+
+var answerSchema = new Schema({
+  user: { type: Schema.Types.ObjectId, ref:'User'},
+  question: { type:Schema.Types.ObjectId, ref: 'Questoin'},
+  answerText: String,
+  upVote: Number,
+  downVote: Number
+});
+
+var User = mongoose.model('User', userSchema);
+var Question = mongoose.model('Question', questionSchema);
+var Answer = mongoose.model('Answer', answerSchema);
