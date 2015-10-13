0d3df99116ec8c6e6475bba6e3d7a65a7d6cf542
EC2 Default User <ec2-user@ip-172-31-19-185.us-west-2.compute.internal>
2015-10-13 03:36:25 +0000

    tmp

--- app/schema.js
+++ app/schema.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var userSchema = new Schema({
-  name: String,
+  name: {type: String, index: {unique: true, dropDups: true}},
   pushId: String,
   questions: [{ type: Schema.Types.ObjectId, ref: 'Question'}],
   answers: [{ type: Schema.Types.ObjectId, ref: 'Answer' }]
